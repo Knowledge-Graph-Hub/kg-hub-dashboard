5311a55844191b875ee4011ed6f4297b1e816709
caufieldjh <j.harry.caufield@gmail.com>
2023-01-10 13:04:36 -0500

    Init!

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+
+REMOTENAME=${1:-origin}
+REMOTE=`git remote get-url --push $ {REMOTENAME}`
+
+# build
+npm run build
+
+cd dist
+
+# If deploying to custom domain
+# echo 'my.domain.org' > CNAME
+
+git init
+git add -A
+git commit -m 'Deploy to gh-pages'
+
+git remote add ${REMOTENAME} ${REMOTE}
+git push --force ${REMOTENAME} main:gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/kg-hub-dashboard/'
+    : '/',
+  chainWebpack: (config) ==> {
+    config.module
+      .rule('markdown')
+      .test(/\.md$/)
+      .use('vue-loader')
+      .end()
+      .use('vmark-loader')
+      .loader('vmark-loader')
+      .end();
+  },
+  lintonsave: false,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'KG-Hub Dashboard',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  }
+};
